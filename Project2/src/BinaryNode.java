bbe6ffc0954a5452281a50c817a380b9ae61b907
jblairkiel <jblairkiel@gmail.com>
2016-02-14 23:27:35 -0600

    Initial Commit for Project 2

---
+++ Project2/src/BinaryNode.java
@@ -0,0 +1,20 @@
+
+public class BinaryNode {
+	
+	//Private
+	String value;
+	BinaryNode left;
+	BinaryNode right;
+
+	/** Constructor for the Binary Node
+	 * 
+	 * @param v The value of the node;
+	 */
+	public BinaryNode(String v){
+		value = v;
+		left = null;
+		right = null;
+	}
+	
+
+}
